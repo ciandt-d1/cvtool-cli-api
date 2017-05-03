bbf355d651a6a83c83db3279e2faf31bf8c5c80e
Gabriel Garcia Bim <gbim@ciandt.com>
2017-05-03 14:31:23 -0300

    removing unecessary code

--- images/kpick-connexion/swagger_server/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-from __future__ import absolute_import
-# import models into model package
-from .annotation import Annotation
-from .image_request import ImageRequest
-from .image_response import ImageResponse
