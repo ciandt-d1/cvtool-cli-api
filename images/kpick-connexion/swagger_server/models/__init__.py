dc2ed6e3b6c646f108c6ffb6594c824f61c42eb6
Gabriel Garcia Bim <gbim@ciandt.com>
2017-05-03 13:25:45 -0300

    generated images api server

---
+++ images/kpick-connexion/swagger_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+# import models into model package
+from .annotation import Annotation
+from .image_request import ImageRequest
+from .image_response import ImageResponse
---
+++ images/kpick-connexion/swagger_server/test/test_image_controller.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from swagger_server.models.image_request import ImageRequest
+from swagger_server.models.image_response import ImageResponse
+from . import BaseTestCase
+from six import BytesIO
+from flask import json
+
+
+class TestImageController(BaseTestCase):
+    """ ImageController integration test stubs """
+
+    def test_add(self):
+        """
+        Test case for add
+
+        
+        """
+        image_request = ImageRequest()
+        query_string = [('tenant_id', 'tenant_id_example'),
+                        ('project_id', 'project_id_example')]
+        response = self.client.open('/v1/images',
+                                    method='POST',
+                                    data=json.dumps(image_request),
+                                    content_type='application/json',
+                                    query_string=query_string)
+        self.assert200(response, "Response body is : " + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
