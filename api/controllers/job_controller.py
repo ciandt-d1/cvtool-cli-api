398e1567a35b0084e250fc04aaefba5d131d3a4a
Gabriel Garcia Bim <gbim@ciandt.com>
2017-05-22 15:35:34 -0300

    including image count for job status

--- api/controllers/job_controller.py
+++ api/controllers/job_controller.py
@@ -71,7 +71,7 @@ def get(tenant_id, job_id):
     :rtype: Job
     """
     job = job_repository.get_by_id(tenant_id, job_id)
-    logger.info(job.flatten())
+    logger.info(str(job.flatten()))
     job_model = Job.from_dict(job.flatten())
     job_model.image_count = job.image_count
     return job_model
